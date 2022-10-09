981407e4b265429a080d752ce6b31f66515e06b5
denis-scherbakov <idenisimus88@gmail.com>
2022-10-09 23:52:15 +0400

    add leaflet

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,3 +1,14 @@
+import { Menu } from "antd";
+
 export function List() {
-  return <div>list</div>;
+  const items = [
+    { label: "item 1", key: "item-1" }, // remember to pass the key prop
+    { label: "item 2", key: "item-2" }, // which is required
+  ];
+
+  return (
+    <div>
+      <Menu items={items} />
+    </div>
+  );
 }
